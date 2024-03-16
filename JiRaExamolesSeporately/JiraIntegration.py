a94ce1585d8dd50e7dfa2a70463984d209446088
aklor <aklorajan@yahoo.com>
2024-03-16 10:48:23 +0330

    test case

--- JiRaExamolesSeporately/JiraIntegration.py
+++ JiRaExamolesSeporately/JiraIntegration.py
@@ -1,12 +1,38 @@
 from jira import JIRA
+from jira.exceptions import JIRAError
 
 
 class JiraIntegration:
     def __init__(self, server, username, api_token, project_key):
-        self.jira = JIRA(server=server, basic_auth=(username, api_token))
+        self.server = server
+        self.username = username
+        self.api_token = api_token
         self.project_key = project_key
 
+        try:
+            self.jira = JIRA(server=server, basic_auth=(username, api_token))
+        except JIRAError as e:
+            raise ValueError(f"Error connecting to Jira: {e}")
+
     def create_test_case(self, summary, description):
-        issue = self.jira.create_issue(project=self.project_key, summary=summary, description=description,
-                                       issuetype={'name': 'Test'})
-        return issue
+        try:
+            issue = self.jira.create_issue(project=self.project_key, summary=summary, description=description,
+                                           issuetype={'name': 'Test'})
+            return issue
+        except JIRAError as e:
+            raise ValueError(f"Error creating test case: {e}")
+
+
+# from jira import JIRA
+#
+#
+# class JiraIntegration:
+#     def __init__(self, server, username, api_token, project_key):
+#         self.jira = JIRA(server=server, basic_auth=(username, api_token))
+#         self.project_key = project_key
+#
+#     def create_test_case(self, summary, description):
+#         issue = self.jira.create_issue(project=self.project_key, summary=summary, description=description,
+#                                        issuetype={'name': 'Test'})
+#         return issue
+
